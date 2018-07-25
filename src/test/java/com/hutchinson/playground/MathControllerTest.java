9013d9c02fbbf4157cbd25ece4a3533433062c02
Christopher Hutchinson <cshutchinson@gmail.com>
2018-07-24 19:47:37 -0600

    Adds /math/pi endpoint

---
+++ src/test/java/com/hutchinson/playground/MathControllerTest.java
@@ -0,0 +1,30 @@
+package com.hutchinson.playground;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(MathController.class)
+public class MathControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void testMathPiEndpoint() throws Exception {
+        mvc.perform(get("/math/pi")
+                .accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("3.141592653589793"));
+    }
+
+}
