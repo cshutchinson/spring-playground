0ef6341933a6f4bc497567db4547c2d7328fa242
Christopher Hutchinson <cshutchinson@gmail.com>
2018-07-24 18:00:49 -0600

    Initial commit

---
+++ src/main/java/com/hutchinson/playground/HelloController.java
@@ -0,0 +1,13 @@
+package com.hutchinson.playground;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello from Spring!";
+    }
+}
