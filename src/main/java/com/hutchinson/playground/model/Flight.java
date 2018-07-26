6d1ca04a748d136b606c6bb4e09d4a1d8b85013a
Christopher Hutchinson <cshutchinson@gmail.com>
2018-07-26 12:05:48 -0600

    Removes unused imports

--- src/main/java/com/hutchinson/playground/model/Flight.java
+++ src/main/java/com/hutchinson/playground/model/Flight.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
-import sun.security.krb5.internal.Ticket;
-
 import java.util.Date;
 import java.util.List;
 
--- src/test/java/com/hutchinson/playground/controller/FlightControllerTest.java
+++ src/test/java/com/hutchinson/playground/controller/FlightControllerTest.java
@@ -7,12 +7,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.hamcrest.Matchers.isEmptyOrNullString;
-import static org.hamcrest.Matchers.nullValue;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(MathController.class)
 class FlightControllerTest {
