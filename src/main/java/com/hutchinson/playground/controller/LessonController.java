178b824e33e0d7c64b72799db78fc68d64b6a9f5
Christopher Hutchinson <cshutchinson@gmail.com>
2018-07-27 15:43:17 -0600

    Adds getOne and delete methods on controller.

--- src/main/java/com/hutchinson/playground/controller/LessonController.java
+++ src/main/java/com/hutchinson/playground/controller/LessonController.java
@@ -4,6 +4,8 @@ import com.hutchinson.playground.model.Lesson;
 import com.hutchinson.playground.repo.LessonRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/lessons")
 public class LessonController {
@@ -21,5 +23,15 @@ public class LessonController {
   @PostMapping("")
   public Lesson create(@RequestBody Lesson lesson){
     return this.lessonRepository.save(lesson);
+  }
+
+  @GetMapping("/{id}")
+  public Optional<Lesson> getOne(@PathVariable Long id){
+    return this.lessonRepository.findById(id);
+  }
+
+  @DeleteMapping("/{id}")
+  public void delete(@PathVariable Long id){
+    this.lessonRepository.deleteById(id);
   }
 }
